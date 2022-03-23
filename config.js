97feec8e6ef74cd8feb35468b4a077fb2d361757
Sourab Patil <sourab054@gmail.com>
2022-03-23 21:11:22 +0530

    Deploy

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const apiUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api" // development api
-    : "https://slice-pizza-4vxo4cr1d-sourab054.vercel.app/api"; // production api
+    : "https://slice-pizza-ordering-app-rjenxbsyi-sourab054.vercel.app/api"; // production api
 
 export { apiUrl };
