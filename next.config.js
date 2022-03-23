cbe24c5ee2f90c31cc580aafa9234c5efb00a825
Sourab Patil <sourab054@gmail.com>
2022-03-23 19:51:09 +0530

    Fixing cors

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/order/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
@@ -20,6 +20,7 @@ module.exports = {
       },
     ];
   },
+  reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
